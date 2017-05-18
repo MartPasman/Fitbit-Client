16581ad743392be70d736d6ff4eb337a0e5eda90
Jeroen Smienk <jeroensmienk@icloud.com>
2017-05-18 09:50:02 +0200

    initial copy

--- src/include/header.php
+++ src/include/header.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <title><?php echo $title ?></title>
 
-    <link href="../css/bootstrap.css" rel="stylesheet">
-    <link href="../css/custom.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
 </head>
 <body>
 
--- src/js/login.js
+++ src/js/login.js
@@ -1,3 +1,55 @@
 /**
  * Created by sveno on 17-5-2017.
  */
+$(document).ready(function () {
+
+    $("#error").hide();
+    $('#login').click(function () {
+        var id = $("#id").val().trim();
+        var password = $("#password").val().trim();
+        var modal = $('#modal-login-error');
+
+
+        $.ajax({
+            url: 'http://localhost:3000/accounts/login',
+            method: 'POST',
+            data: {
+                id: id,
+                password: password
+            },
+            statusCode: {
+                201: function (data) {
+                    console.dir(data.token);
+                    localStorage["token"] = data.success;
+                    if(data.permission>1){
+                        location.replace();
+                    }else{
+                        location.replace();
+                    }
+                },
+                400: function (err) {
+                    $("#error").toggle('show');
+                },
+                500: function (err) {
+                    // set the modal title
+                    modal.find('.modal-title').html('Er is iets misgegaan!');
+                    // set the modal body
+                    modal.find('.modal-body').html("Het is niet jou fout, probeer het later nog eens");
+                    // show the modal
+                    modal.modal();
+                },
+                default: function (err) {
+                    // set the modal title
+                    modal.find('.modal-title').html('Er is iets misgegaan!');
+                    // set the modal body
+                    modal.find('.modal-body').html("Probeer het nog eens");
+                    // show the modal
+                    modal.modal();
+                }
+            }
+        });
+    });
+});
+
+
+
