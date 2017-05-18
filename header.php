4d3843e14d1d8366a95ceda2ac504717e6eefb6d
Jeroen <jeroensmienk@icloud.com>
2017-05-18 09:47:27 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $title ?></title>
+
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#important-id-for-collapsing" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><img class="icon" src="../img/go.png"/> </a>
+        </div>
+        <div class="collapse navbar-collapse" id="important-id-for-collapsing">
+            <ul class="nav navbar-nav">
+                <li><a href="#">Link #1</a></li>
+                <li><a href="#">Link #2</a></li>
+                <li><a href="#">Link #3</a></li>
+            </ul>
+        </div>
+
+        <span class="led-bad"></span>
+    </div>
+</nav>
