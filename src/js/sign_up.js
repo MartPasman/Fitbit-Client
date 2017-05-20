4f380cdbe49a0b2dd50452ab8d800aa3d666df6f
romybeugeling <romybeugeling@live.nl>
2017-05-20 20:11:23 +0200

    us05 quickfix

--- src/js/sign_up.js
+++ src/js/sign_up.js
@@ -103,7 +103,7 @@ $(document).ready(function () {
 
 
             $.ajax({
-                url: 'http://localhost:3000/accounts/users',
+                url: REST + '/accounts/users',
                 method: 'POST',
                 headers: {
                     Authorization: localStorage.getItem('token')
