27c26561f1bb5317cf836be0a35da609d8e65954
Jeroen Smienk <jeroensmienk@icloud.com>
2017-06-10 16:28:44 +0200

    minor fixes

--- src/user-settings.php
+++ src/user-settings.php
@@ -23,97 +23,88 @@ include './include/header.php';
                 <i class="icon ion-help-circled" rel="tooltip" title="Hint"></i>
             </div>
         </ul>
-    </div>
 
-    <div class="row">
-        <div class="tab-content">
+        <div class="row">
+            <div class="tab-content">
 
-            <!-- My personal data -->
-            <div id="personal" class=" tab-pane fade  in active" style="margin-top:30px;">
-                <div class="col-sm-6 col-sm-offset-3 col-xs-12">
-                    <div class="panel panel-default">
+                <!-- My personal data -->
+                <div id="personal" class="tab-pane fade in active">
+                    <div class="col-xs-12">
+                        <div class="panel panel-default">
 
-                        <div class="panel-heading"><h2>Mijn gegevens</h2></div>
-                        <div class="panel-body">
-                            <div class="alert alert-danger" role="alert" id="error">
-                                <strong>Oeps!</strong> Er ging iets mis.
+                            <div class="panel-heading"><h2>Mijn gegevens</h2></div>
+                            <div class="panel-body row">
+                                <div class="col-xs-12 col-md-6 col-md-offset-3">
+                                    <div class="alert alert-danger" role="alert" id="error">
+                                        <strong>Oeps!</strong> Er ging iets mis.
+                                    </div>
+                                    <div class="alert alert-success" role="alert" id="success">
+                                        <strong>Gelukt!</strong> Je informatie is nu geupdated.
+                                    </div>
+
+                                    <div id="id-field"></div>
+                                    <br/><br/>
+                                    <form>
+                                        <div id="name-field"></div>
+                                        <br/><br/>
+                                        <div id="email-field"></div>
+                                        <br/><br/>
+                                        <div id="date-field"></div>
+                                        <br/><br/>
+                                        <div id="age-field"></div>
+                                        <br/><br/>
+                                        <div id="health-field"></div>
+                                        <br/><br/>
+                                        <div id="buttons"></div>
+                                    </form>
+                                </div>
                             </div>
-                            <div class="alert alert-success" role="alert" id="success">
-                                <strong>Gelukt!</strong> Je informatie is nu geupdated.
-                            </div>
-                            <div id="id-field"></div>
-                            <p></p>
-                            <form>
-                                <div id="name-field">
-
-                                </div>
-                                <p></p>
-                                <div id="email-field">
-
-                                </div>
-                                <p></p>
-                                <div id="date-field">
-
-                                </div>
-                                <p></p>
-                                <div id="age-field">
-
-                                </div>
-                                <p></p>
-                                <div id="health-field">
-
-                                </div>
-                                <p></p>
-                                <div id="buttons">
-
-                                </div>
-                            </form>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <!-- My goals data -->
-            <div id="goals" class="tab-pane fade" style="margin-top:30px;">
-                <div class="col-sm-6 col-sm-offset-3 col-xs-12">
-                    <!-- Goal data -->
-                    <div class="panel panel-default">
-                        <div class="panel-heading"><h2>Doelstellingen</h2></div>
-                        <div class="panel-body"> In dit vak staan je instellingen wat betreft je doelstellingen. Je kan
-                            een
-                            nieuwe
-                            doelstelling aanmaken,
-                            of een lopende verwijderen.
-                            <p></p>
-                            <h5><b>Voeg een nieuwe doelstelling toe:</b></h5>
-                            <p></p>
-                            <button type="button" data-toggle="modal" data-target="#form-modal"
-                                    class="col-xs-12 btn btn-success"
-                                    id="new-goal">
-                                Nieuwe doelstelling
-                            </button>
-                            <p></p>
-                            <h5><b>Alle doelstellingen:</b></h5>
-                            <p></p>
-                            <button type="button" data-toggle="modal" data-target="#item-modal"
-                                    class="col-xs-12 btn btn-success"
-                                    id="all-goals">
-                                Doelstellingen
-                            </button>
+                <!-- My goals data -->
+                <div id="goals" class="tab-pane fade">
+                    <div class="col-xs-12">
+                        <!-- Goal data -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading"><h2>Doelstellingen</h2></div>
+                            <div class="panel-body row">
+                                <div class="col-xs-12 col-md-6 col-md-offset-3">
+                                    In dit vak staan je instellingen wat betreft je doelstellingen.
+                                    Je kan een nieuwe doelstelling aanmaken, of een lopende verwijderen.
+                                    <br/><br/>
+
+                                    <h5>Voeg een nieuwe doelstelling toe:</h5>
+                                    <button type="button" data-toggle="modal" data-target="#form-modal"
+                                            class="col-xs-12 btn btn-success" id="new-goal">
+                                        Nieuwe doelstelling
+                                    </button>
+                                    <br/><br/>
+
+                                    <h5>Alle doelstellingen:</h5>
+                                    <button type="button" data-toggle="modal" data-target="#item-modal"
+                                            class="col-xs-12 btn btn-success" id="all-goals">
+                                        Doelstellingen
+                                    </button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <!-- Settings data -->
-            <div id="settings" class="tab-pane fade" style="margin-top:30px;">
-                <div class="col-sm-6 col-sm-offset-3 col-xs-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading"><h2>Wachtwoord</h2></div>
-                        <div class="panel-body">
-                            <?php
-                            include './include/change-password.php';
-                            ?>
+                <!-- Settings data -->
+                <div id="settings" class="tab-pane fade">
+                    <div class="col-xs-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading"><h2>Wachtwoord</h2></div>
+                            <div class="panel-body row">
+                                <div class="col-xs-12 col-md-6 col-md-offset-3">
+                                    <?php
+                                    include './include/change-password.php';
+                                    ?>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
