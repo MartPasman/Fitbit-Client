be3e501148eb907ee4eb42a0a9b022161416dd06
Jeroen Smienk <jeroensmienk@icloud.com>
2017-06-12 14:15:31 +0200

    layout Social.php

---
+++ src/social.php
@@ -0,0 +1,40 @@
+<?php
+$title = "Mijn resultaten";
+include './include/header.php';
+?>
+
+    <!-- today -->
+    <div id="today"></div>
+
+    <div class="container-fluid">
+
+        <!-- Activity data -->
+        <div id="activity-data" class="col-sm-4">
+            <div class="block">
+                <h2>Nieuws</h2>
+            </div>
+        </div>
+
+        <!-- Activity data -->
+        <div id="activity-data" class="col-sm-4">
+            <div class="block">
+                <h2>Weer</h2>
+            </div>
+        </div>
+
+        <!-- Activity data -->
+        <div id="activity-data" class="col-sm-4">
+            <div class="block">
+                <h2>Verjaardagen</h2>
+            </div>
+        </div>
+    </div>
+
+    <!-- JQuery and Bootstrap scripts -->
+<?php include './include/scripts.php'; ?>
+
+    <!-- your scripts -->
+
+
+    <!-- end of body -->
+<?php include './include/footer.php'; ?>
