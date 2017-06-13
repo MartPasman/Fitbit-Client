004cd0b3a856a94fa24f7d26008da66b6082481b
romybeugeling <romybeugeling@live.nl>
2017-06-13 20:26:00 +0200

    kut

--- src/js/birthday.js
+++ src/js/birthday.js
@@ -16,14 +16,12 @@ $(document).ready(function () {
     const dayFrom = new Date().setDate(new Date().getDate() - (day - 1));
     const dayTo = new Date().setDate(new Date().getDate() + (7 - day));
 
-
     $.ajax({
         url: REST + '/users/birthdays',
         method: 'GET',
         statusCode: {
             200: function (data) {
-
-                console.dir(data.success);
+                loadBirthdays(data.success);
             },
             400: function (err) {
             },
@@ -37,3 +35,58 @@ $(document).ready(function () {
 
     });
 });
+
+
+ function loadBirthdays(users) {
+    let listBirthdays = $("#birthdays");
+    let noBday = $("#no-birthdays");
+    noBday.hide();
+
+    // if (users.length === 0) {
+    //     noBday.show();
+ //    } else {
+ //
+ //        for (let i = 0; i < users.length; i++) {
+ //            let user = users[i];
+ //            let birthdayHTML;
+ //            //TODO blah blah is morgen jarig, hoe deed jeroen dat ook alweer
+ //            //TODO lange naam fixen layout
+ //
+ //
+ //            // let dd = user.birthday.split("-");
+ //            // console.log( dd[0] + " ," + dd[1] + " ," + dd[2]);
+ //            // let dddd = dd[2].split(0, 1);
+ //            // console.log(dddd[0]);
+ //
+ //
+ //            if (user.birthday === new Date()) {
+ //
+ //                birthdayHTML = "<div class='col-xs-12 col-md-12 birthday-today'> " +
+ //                    user.firstname + " " + user.lastname +
+ //                    "</div><img src='img/birthday-cake.png' alt='Gefeliciteerd' height='80' width='80'></div>"
+ //
+ //
+ //            } else {
+ //                // let bday = getCompareDate(user.birthday);
+ //                // let days = getDaysBetween(today, bday);
+ //                // console.dir(user.birthday);
+ //
+ //                birthdayHTML = "<div class='col-xs-12 col-md-12 birthday-one-user'>" + user.firstname
+ //                    + " " + user.lastname + " " + user.birthday +
+ //                    "<img src='img/almost.png' id='birthday-cake' alt='Gefeliciteerd' height='60' width='60'></div> ";
+ //            }
+ //
+ //            let html = "<div class='birthday-users row' >" +
+ //                "<div class='col-xs-12 col-md-12 birthday-user' " +
+ //                birthdayHTML + "</div> </div> <hr/>";
+ //
+ //            listBirthdays.append(html);
+ //        }
+ //    }
+//
+}
+
+
+function getCompareDate(birthdate) {
+    return compareDate = Date.UTC(new Date().getFullYear(), birthdate.getMonth(), birthdate.getDate(), 0, 0, 0, 0);
+}
--- src/social.php
+++ src/social.php
@@ -22,18 +22,43 @@ include './include/header.php';
             </div>
         </div>
 
-        <!-- Activity data -->
-        <div id="activity-data" class="col-sm-4">
-            <div class="block">
+        <!-- Birthdays-->
+        <div id="activity-data " class="col-sm-4">
+            <div class="block" id="birthdays">
                 <h2>Verjaardagen</h2>
+                <hr/>
+
+                <h3 id="no-birthdays">Niemand is jarig deze week</h3>
+
+                <div class='birthday-user row'>
+                    <div class='col-xs-12 col-md-12  birthday-today'> Romy Beugeling is vandaag jarig
+                        <img src="img/birthday-cake.png" id="birthday-cake" alt="Gefeliciteerd" height="80" width="80">
+                    </div>
+
+                </div>
+                <hr/>
+
+                <div class='birthday-user row'>
+                    <div class='col-xs-12 col-md-12  birthday-user'> Romy Beugeling is morgen jarig
+                        <img src="img/almost.png" id="birthday-cake" alt="Gefeliciteerd" height="60" width="60">
+                    </div>
+
+                </div>
+                <hr/>
+
+                <!-- TODO max 7 mensen per week -->
+
             </div>
         </div>
+    </div>
     </div>
 
     <!-- JQuery and Bootstrap scripts -->
 <?php include './include/scripts.php'; ?>
 
     <!-- your scripts -->
+    <script src="js/birthday.js"></script>
+    <link rel="stylesheet" href="css/social.css">
 
 
     <!-- end of body -->
