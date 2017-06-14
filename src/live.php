8f0c4e010e8b7c25319326e1e2138afd6713e2b4
Jeroen Smienk <jeroensmienk@icloud.com>
2017-06-14 14:10:13 +0200

    adam 4k

--- src/live.php
+++ src/live.php
@@ -1,12 +1,20 @@
 <?php
-$title = "Mijn resultaten";
+$title = 'Live';
 include './include/header.php';
+
+// Amsterdam Dam
+$amsterdam = 'https://www.youtube.com/embed/vpm16w3ik0g';
+
+// Elephants
+$elephants = 'https://www.youtube.com/embed/BaS2ofdeHPU';
+
+$url = $amsterdam;
 ?>
     <!-- today -->
     <div id="today-cover"></div>
 
     <iframe id="live-player" class="youtube_player" type="text/html" frameborder="0"
-            src="https://www.youtube.com/embed/BaS2ofdeHPU?vq=hd720&amp;autoplay=1&amp;autohide=1&amp;controls=0&amp;rel=0&amp;fs=1&amp;wmode=transparent&amp;showinfo=0&amp;modestbranding=0&amp;theme=dark&amp;color=red&amp;enablejsapi=1&amp;html5=1"
+            src="<?php echo $url; ?>?vq=hd1080&amp;autoplay=1&amp;autohide=1&amp;controls=1&amp;rel=0&amp;fs=1&amp;wmode=transparent&amp;showinfo=0&amp;modestbranding=0&amp;theme=dark&amp;color=red&amp;enablejsapi=1&amp;html5=1"
             allowfullscreen=""
             style="width: calc(100vw); height: calc(100vh - 50px);">
 
