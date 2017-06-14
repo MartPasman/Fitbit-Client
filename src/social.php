e4b26349236af3e242038f24f39a65102d627c57
Mart Pasman <mart_pasman@hotmail.com>
2017-06-14 08:59:51 +0200

    BUG FIXES

--- src/social.php
+++ src/social.php
@@ -6,8 +6,14 @@ include './include/header.php';
     <!-- today -->
     <div id="today"></div>
 
-    <div class="container-fluid">
 
+    <div class="container-fluid">
+        <div id="bar-div" class="progress" style="margin-right: 2%; margin-left:2%; height: 40px;">
+            <div id="progressBar" class="progress-bar progress-bar-danger  progress-bar-striped active" role="progressbar"
+                 aria-valuemin="0" aria-valuemax="100" style="width:45%">
+                <h4 id="percentage"></h4>
+            </div>
+        </div>
         <!-- Activity data -->
         <div id="activity-data" class="col-sm-4">
             <div class="block">
@@ -34,6 +40,7 @@ include './include/header.php';
 <?php include './include/scripts.php'; ?>
 
     <!-- your scripts -->
+    <script src="js/social.js" type="text/javascript"></script>
 
 
     <!-- end of body -->
