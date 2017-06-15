4c72f44472603d117229604eb1f865c32f3a9e60
Mart Pasman <mart_pasman@hotmail.com>
2017-06-15 12:13:49 +0200

    BUG FIXES

--- src/js/social.js
+++ src/js/social.js
@@ -19,11 +19,12 @@ $(document).ready(function () {
                     let totWidth = $('#bar-div').width();
                     let width = totWidth * data.success.percentage / 100;
                     progressBar.width(width);
-                    $('#percentage').text(data.success.percentage + '%');
-                    if(data.success.achieved){
-                        progressBar.css('background-color', 'green');
+                    if(data.success.percentage > 20 ) {
+                        $('#percentage').text(data.success.percentage + '%');
+                        if (data.success.achieved) {
+                            progressBar.css('background-color', 'green');
+                        }
                     }
-
                 },
                 default: function (err) {
                     console.log(err.message);
