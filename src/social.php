5a4cedae73a408406583644b2299cd7538c6ff38
svenordelman <sveno98@live.nl>
2017-06-15 13:36:22 +0200

    social-dashboard

--- src/social.php
+++ src/social.php
@@ -2,8 +2,9 @@
 $title = "Mijn resultaten";
 include './include/header.php';
 ?>
-    <link rel="stylesheet"
-          href="./css/weather.css"/>
+    <link rel="stylesheet" href="./css/weather.css"/>
+    <link rel="stylesheet" href="./css/weather-icons.css"/>
+
     <!-- today -->
     <div id="today"></div>
 
@@ -28,16 +29,16 @@ include './include/header.php';
         <div id="activity-data" class="col-sm-4">
             <div class="weather-card">
                 <div class="top">
-                    <div class="wrapper">
-                        <h1 class="heading">Zonnig</h1>
-                        <h3 class="location">Enschede, Nederland</h3>
+                       <div class="wrapper">
+                           <i id="today-icon" style="font-size: 7em; float:left; position:absolute; left:5%; color:white;"></i>
+                           <h1 class="heading" id="condition"></h1>
+                        <h3 class="location" id="location"></h3>
                         <p class="temp">
-                            <span class="temp-value">17</span>
+                            <span class="temp-value" id="curtemp"></span>
                             <span class="deg">0</span>
                             <a href="javascript:;"><span class="temp-type">C</span></a>
                         </p>
-                        <span class="temp-small">24/11<span class="deg">0</span><span
-                                    class="temp-type">C</span></span>
+                        <span class="temp-small" id="today-max-min"></span>
                     </div>
                 </div>
                 <div class="bottom">
@@ -47,15 +48,13 @@ include './include/header.php';
                             <li class="active">
                                 <span class="date">Morgen</span>
                                 <span class="lnr lnr-sun condition">
-									<span class="temp">18/13<span class="deg">0</span><span
-                                                class="temp-type">C</span></span>
+									<span class="temp" id="tom-max-min"></span>
 								</span>
                             </li>
                             <li>
                                 <span class="date">Overmorgen</span>
                                 <span class="lnr lnr-cloud condition">
-									<span class="temp">18/11<span class="deg">0</span><span
-                                                class="temp-type">C</span></span>
+									<span class="temp" id="tom2-max-min"></span>
 								</span>
                             </li>
                         </ul>
