2007d21eefe04c9636d9efa93905a621b4b69cb7
Jeroen Smienk <jeroensmienk@icloud.com>
2017-06-21 11:06:55 +0200

    fix

--- src/js/competition/competition-dashboard.js
+++ src/js/competition/competition-dashboard.js
@@ -262,15 +262,8 @@ function startTimeResize() {
     let h = today.getHours();
     let m = today.getMinutes();
     let s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
+    s = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
     $('#time').html(h + ":" + m + ":" + s);
     let t = setTimeout(startTimeResize, 1000);
-}
-function checkTime(i) {
-    if (i < 10) {
-        i = "0" + i
-    }
-    ;  // add zero in front of numbers < 10
-    return i;
 }
